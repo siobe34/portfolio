ac1bf3c572651648b3fa3b64fdda0c221230c753
siobe <siobe14@hotmail.com>
2022-04-11 15:42:43 -0400

    v0.7.3: modularized routes/controllers/services

--- api/src/controllers/movie.controller.ts
+++ api/src/controllers/movie.controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from 'express';
-import MovieModel from '../models/movie.model';
+import movieServices from '../services/movie.service';
 
-exports.createMovie = (req: Request, res: Response) => {
-    res.send('Add a new movie to the database.');
+exports.handleMovieQuery = (req: Request, res: Response) => {
+    res.send(movieServices.getMovie());
+}
+
+exports.handleMovieCreation = (req: Request, res: Response) => {
+    const movieCreationStatus  = movieServices.createMovie(req.body);
+    
+    if (movieCreationStatus) {
+        res.send(`Added a new movie to the database with:\nTitle: ${req.body.title}\nRanking: ${req.body.ranking}`);
+        return;
+    };
+    
+    res.send('Could not create the new movie.');
 };
 
 export default exports;
--- api/src/routes/media.route.ts
+++ api/src/routes/media.route.ts
@@ -1,50 +1,11 @@
-import { Request, Response } from 'express';
 import express from 'express';
 
 import movieController from '../controllers/movie.controller';
-type DataTypes = {
-    movies: string[];
-    anime: string[];
-    shows: string[];
-    music: string[];
-    [key: string]: string[]
-}
-const dummyData: DataTypes = {
-    movies: [
-        'Movie Example 1',
-        'Movie Example 2',
-        'Movie Example 3',
-        'Movie Example 4'
-    ],
-    anime: [
-        'Anime Example 1',
-        'Anime Example 2',
-        'Anime Example 3',
-        'Anime Example 4'
-    ],
-    shows: [
-        'Show Example 1',
-        'Show Example 2',
-        'Show Example 3',
-        'Show Example 4'
-    ],
-    music: [
-        'Music Example 1',
-        'Music Example 2',
-        'Music Example 3',
-        'Music Example 4'
-    ]
-}
-
 
 const router = express.Router();
 
-router.get('/:mediaType', (req: Request, res: Response) => {
-    const mediaType: keyof DataTypes =   req.params.mediaType;
-    if (!Object.keys(dummyData).includes(mediaType)) res.sendStatus(404);
-    res.send(dummyData[mediaType]);
-});
+router.get('/movies', movieController.handleMovieQuery);
 
-router.post('/:mediaType', movieController.createMovie);
+router.post('/:mediaType', movieController.handleMovieCreation);
 
 export default router;
