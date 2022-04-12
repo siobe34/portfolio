e8b5b26f317e22594b2e36330d1993320b9315a6
siobe <siobe14@hotmail.com>
2022-04-12 18:14:41 -0400

    v0.10.0: revised requests to backend

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,23 +16,78 @@ type PropTypes = {
     setTheme: React.Dispatch<React.SetStateAction<string>>;
 };
 
+const styles = {
+    mediaButton: {
+        regular: {
+            width: '100%',
+            backgroundColor: 'var(--primary)',
+            whiteSpace: 'revert' as 'revert',
+            gap: '0.5rem',
+        },
+        highlight: {
+            width: '100%',
+            backgroundColor: 'var(--secondary)',
+            whiteSpace: 'revert' as 'revert',
+            gap: '0.5rem',
+        },
+    },
+    recommendationContainer: {
+        container: {
+            width: '100%',
+            justifyContent: 'center',
+            gap: '1rem',
+        },
+        button: {
+            width: '100%',
+            fontSize: '1.2rem',
+        },
+    },
+    recommendationInput: {
+        container: {
+            margin: '1rem 0',
+            padding: '1rem 2rem',
+            color: 'var(--bg-primary-txt)',
+            backgroundColor: 'var(--secondary)',
+            borderRadius: '.2rem',
+        },
+        input: {
+            width: '200px',
+            outline: 'none',
+            color: 'inherit',
+            backgroundColor: 'inherit',
+            border: 'none',
+            textAlign: 'center' as 'center',
+            fontWeight: 'bold',
+            textTransform: 'uppercase' as 'uppercase',
+        },
+    },
+};
+
 function UnstyledFooter({ className, setTheme }: PropTypes) {
     const theme = useContext(ThemeContext);
 
     const [modalState, setModalState] = useState<boolean>(false);
+    const [media, setMedia] = useState<string | null>(null);
     const [recommendation, setRecommendation] = useState<string | null>(null);
 
     const randomInteger = (min: number, max: number) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    async function getRecommendation(mediaType: string) {
-        const opts = {
-            method: 'GET',
+    async function getRecommendation(method: string) {
+        let opts: RequestInit = {
+            method: method,
             headers: {
                 'Content-Type': 'application/json',
             },
         };
-        const response = await fetch(`http://127.0.0.1:1337/media/${mediaType}`, opts);
+        if (method === 'POST' && recommendation !== null) {
+            opts = {
+                ...opts,
+                body: JSON.stringify({ title: recommendation }),
+            };
+        }
+
+        const response = await fetch(`http://127.0.0.1:1337/media/${media}`, opts);
         const json = await response.json();
 
         if (response.status === 200 && json) {
@@ -45,7 +100,14 @@ function UnstyledFooter({ className, setTheme }: PropTypes) {
 
     return (
         <Container className={`footer ${className}`} containerStyle={{ justifyContent: 'space-between' }}>
-            <Button onButtonClick={() => setModalState(!modalState)} buttonStyle={{ color: 'red' }}>
+            <Button
+                onButtonClick={() => {
+                    setMedia(null);
+                    setRecommendation(null);
+                    setModalState(!modalState);
+                }}
+                buttonStyle={{ color: 'red' }}
+            >
                 <FontAwesomeIcon icon={faHeart} />
             </Button>
             <a href='https://github.com/siobe34' className='footer-txt'>
@@ -60,7 +122,7 @@ function UnstyledFooter({ className, setTheme }: PropTypes) {
                 Switch to {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
                 <FontAwesomeIcon icon={faStarHalfStroke} className={theme === 'dark' ? 'star-left' : 'star-right'} style={{ marginLeft: '5px' }} />
             </Button>
-            <Modal modalState={modalState} setModalState={setModalState}>
+            <Modal modalState={modalState} setModalState={setModalState} modalStyle={{ height: 'fit-content' }}>
                 <Header headerType='h1'>Media Recommendations</Header>
                 <Container
                     containerStyle={{
@@ -86,44 +148,54 @@ function UnstyledFooter({ className, setTheme }: PropTypes) {
                         </Span>
                     </p>
                 </Container>
+                <Header headerType='h3'>Select a type of media:</Header>
                 <Container
                     containerStyle={{
                         gap: '1rem',
-                        margin: '3rem 0',
+                        margin: '1rem 0 2rem 0',
                         width: '100%',
                     }}
                 >
-                    <Button buttonStyle={{ width: '100%', whiteSpace: 'revert' }} onButtonClick={() => getRecommendation('movies')}>
-                        <FontAwesomeIcon icon={faClapperboard} style={{ marginRight: '0.5rem' }} />
+                    <Button
+                        buttonStyle={media === 'movies' ? styles.mediaButton.highlight : styles.mediaButton.regular}
+                        onButtonClick={() => setMedia('movies')}
+                    >
+                        <FontAwesomeIcon icon={faClapperboard} />
                         Movies
                     </Button>
-                    <Button buttonStyle={{ width: '100%', whiteSpace: 'revert' }} onButtonClick={() => getRecommendation('shows')}>
-                        <FontAwesomeIcon icon={faTv} style={{ marginRight: '0.5rem' }} />
+                    <Button
+                        buttonStyle={media === 'shows' ? styles.mediaButton.highlight : styles.mediaButton.regular}
+                        onButtonClick={() => setMedia('shows')}
+                    >
+                        <FontAwesomeIcon icon={faTv} />
                         Shows
                     </Button>
-                    <Button buttonStyle={{ width: '100%', whiteSpace: 'revert' }} onButtonClick={() => getRecommendation('anime')}>
-                        <FontAwesomeIcon icon={faZap} style={{ marginRight: '0.5rem' }} />
+                    <Button
+                        buttonStyle={media === 'anime' ? styles.mediaButton.highlight : styles.mediaButton.regular}
+                        onButtonClick={() => setMedia('anime')}
+                    >
+                        <FontAwesomeIcon icon={faZap} />
                         Anime
                     </Button>
-                    <Button buttonStyle={{ width: '100%', whiteSpace: 'revert' }} onButtonClick={() => getRecommendation('music')}>
-                        <FontAwesomeIcon icon={faMusic} style={{ marginRight: '0.5rem' }} />
+                    <Button
+                        buttonStyle={media === 'music' ? styles.mediaButton.highlight : styles.mediaButton.regular}
+                        onButtonClick={() => setMedia('music')}
+                    >
+                        <FontAwesomeIcon icon={faMusic} />
                         Music
                     </Button>
                 </Container>
-                {recommendation && (
-                    <Span
-                        spanStyle={{
-                            color: 'var(--bg-primary-txt)',
-                            backgroundColor: 'var(--secondary)',
-                            borderRadius: '.2rem',
-                            padding: '1rem 2rem',
-                        }}
-                    >
-                        <Header headerType='h4' headerStyle={{ textTransform: 'uppercase' }}>
-                            {recommendation}
-                        </Header>
-                    </Span>
-                )}
+                <Container containerStyle={styles.recommendationContainer.container}>
+                    <Button buttonStyle={styles.recommendationContainer.button} onButtonClick={() => getRecommendation('GET')}>
+                        Get a Recommendation
+                    </Button>
+                    <Button buttonStyle={styles.recommendationContainer.button} onButtonClick={() => getRecommendation('POST')}>
+                        Give a Recommendation
+                    </Button>
+                </Container>
+                <Container containerStyle={styles.recommendationInput.container}>
+                    <input defaultValue={recommendation ?? ''} placeholder='Type a Recommendation' style={styles.recommendationInput.input} />
+                </Container>
             </Modal>
         </Container>
     );
