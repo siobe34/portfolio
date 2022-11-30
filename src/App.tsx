b15b810c099fefaa57d304c3631d5e764973e88e
siobe34 <siobe14@hotmail.com>
2022-11-30 01:49:16 -0500

    added Source Code Pro font with webfontloader

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
     useEffect(() => {
         webFont.load({
             google: {
-                families: ["Montserrat: 100,200,300,400,500,600,700,800,900"],
+                families: ["Montserrat: 100,200,300,400,500,600,700,800,900", "Source Code Pro: 100,200,300,400,500,600,700,800,900"],
             },
         });
     }, []);
