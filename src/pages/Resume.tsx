1a1a7951da585a016547bd64dcaacab8425b70b1
siobe34 <siobe14@hotmail.com>
2023-02-22 12:36:33 -0500

    implemented download Resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,13 +1,24 @@
 import { Button } from "../components/Button";
 import { Card } from "../components/Card";
 
+const resumeFile = "/Resume_Ibad-Rashid.pdf";
 function Resume() {
+    // * Func to create anchor tag, set the download prop, and click the anchor tag
+    const downloadFile = () => {
+        const anchorDownload = document.createElement("a");
+        anchorDownload.href = resumeFile;
+        anchorDownload.download = resumeFile;
+        anchorDownload.click();
+    };
+
     return (
         <>
             <h1 className='my-8'>Resume</h1>
             <Card className='flex-col gap-8 p-16'>
-                <Button>Download PDF</Button>
-                <h3>Unfortunately the download button doesn't work as of right now.</h3>
+                <Button>
+                    <a href={resumeFile}>View in Browser</a>
+                </Button>
+                <Button onClick={() => downloadFile()}>Download PDF</Button>
             </Card>
         </>
     );
