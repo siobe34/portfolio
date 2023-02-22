ff2613877ccf39a5a426e989f4b6c6f2359fd973
siobe34 <siobe14@hotmail.com>
2023-02-22 10:09:31 -0500

    uncluttered technologies

--- src/pages/Technologies.tsx
+++ src/pages/Technologies.tsx
@@ -24,10 +24,6 @@ function Technologies() {
                                 <ListItem className={listStyle}>
                                     Leveraged Google Maps JS API to create custom components to represent various geospatial assets.
                                 </ListItem>
-                                <ListItem className={listStyle}>
-                                    Designed relational database to seamlessly connect different types of geospatial assets.
-                                </ListItem>
-                                <ListItem className={listStyle}>Implemented responsive UI to manage the database of geospatial assets.</ListItem>
                             </List>
                         </Collapsible>
                     </div>
@@ -38,9 +34,11 @@ function Technologies() {
                         <Collapsible>
                             <></>
                             <List className='list-disc'>
-                                <ListItem className={listStyle}>Started learning JS Frameworks with React.</ListItem>
-                                <ListItem className={listStyle}>Currently learning Next.js</ListItem>
-                                <ListItem className={listStyle}>Hoping to one day learn React Native.</ListItem>
+                                <ListItem className={listStyle}>Experience with pure React, React Frameworks, and various CSS solutions.</ListItem>
+                                <ListItem className={listStyle}>Familiarity with Next.js</ListItem>
+                                <ListItem className={listStyle}>Preference for Tailwind CSS</ListItem>
+                                <ListItem className={listStyle}>Styled Components</ListItem>
+                                <ListItem className={listStyle}>Bootstrap</ListItem>
                             </List>
                         </Collapsible>
                     </div>
@@ -67,9 +65,6 @@ function Technologies() {
                                 <ListItem className={listStyle}>React</ListItem>
                                 <ListItem className={listStyle}>React Router</ListItem>
                                 <ListItem className={listStyle}>Node Express</ListItem>
-                                <ListItem className={listStyle}>Styled Components</ListItem>
-                                <ListItem className={listStyle}>Tailwind CSS</ListItem>
-                                <ListItem className={listStyle}>Bootstrap</ListItem>
                                 <ListItem className={listStyle}>JQuery</ListItem>
                             </List>
                         </Collapsible>
@@ -81,7 +76,7 @@ function Technologies() {
                         <Collapsible>
                             <></>
                             <List className='list-disc'>
-                                <ListItem className={listStyle}>Flask</ListItem>
+                                <ListItem className={listStyle}>Built restful API's with Flask</ListItem>
                                 <ListItem className={listStyle}>Pandas</ListItem>
                                 <ListItem className={listStyle}>SQL Alchemy</ListItem>
                             </List>
@@ -95,9 +90,9 @@ function Technologies() {
                             <></>
 
                             <List className='list-disc'>
-                                <ListItem className={listStyle}>My SQL</ListItem>
-                                <ListItem className={listStyle}>Maria DB</ListItem>
-                                <ListItem className={listStyle}>Mongo DB</ListItem>
+                                <ListItem className={listStyle}>MySQL</ListItem>
+                                <ListItem className={listStyle}>MariaDB</ListItem>
+                                <ListItem className={listStyle}>MongoDB</ListItem>
                                 <ListItem className={listStyle}>SQLite</ListItem>
                             </List>
                         </Collapsible>
@@ -111,9 +106,6 @@ function Technologies() {
                             <List className='list-disc'>
                                 <ListItem className={listStyle}>Designed endpoints adhering to RestAPI best practices.</ListItem>
                                 <ListItem className={listStyle}>Implemented endpoints with CRUD operations.</ListItem>
-                                <ListItem className={listStyle}>
-                                    Used test-driven development approach to develop endpoints with standard HTTP response codes.
-                                </ListItem>
                             </List>
                         </Collapsible>
                     </div>
@@ -127,7 +119,7 @@ function Technologies() {
                                 <ListItem className={listStyle}>Reverse proxy for various open-source services in homelab environment.</ListItem>
                                 <ListItem className={listStyle}>Protected with TLS/SSL certificates.</ListItem>
                                 <ListItem className={listStyle}>Configured personal wildcard domain to host internal services.</ListItem>
-                                <ListItem className={listStyle}>Spun up with Portainer using Docker.</ListItem>
+                                <ListItem className={listStyle}>Deployed with Portainer using Docker.</ListItem>
                             </List>
                         </Collapsible>
                     </div>
@@ -157,23 +149,6 @@ function Technologies() {
                         </Collapsible>
                     </div>
                 </Card>
-                <Card
-                    className={cardStyle}
-                    image={Logos.projectmanagement}
-                    imageSize={{ x: "64px", y: "64px" }}
-                    imageClassName='dark:bg-white rounded p-1'
-                >
-                    <div className='flex flex-col gap-1'>
-                        <h3 className='text-center'>Project Management</h3>
-                        <Collapsible>
-                            <></>
-                            <List className='list-disc'>
-                                <ListItem className={listStyle}>Client Correspondence</ListItem>
-                                <ListItem className={listStyle}>Technical Writing</ListItem>
-                            </List>
-                        </Collapsible>
-                    </div>
-                </Card>
                 <Card className={cardStyle} image={Logos.github} imageSize={{ x: "64px", y: "64px" }} imageClassName='dark:bg-white rounded p-1'>
                     <div className='flex flex-col gap-1'>
                         <h3 className='text-center'>GitHub</h3>
@@ -181,39 +156,6 @@ function Technologies() {
                             <></>
                             <List className='list-disc'>
                                 <ListItem className={listStyle}>Used Git for version control of personal projects.</ListItem>
-                            </List>
-                        </Collapsible>
-                    </div>
-                </Card>
-
-                <Card className={cardStyle} image={Logos.HTML} imageSize={{ x: "64px", y: "64px" }} imageClassName='dark:bg-white rounded p-1'>
-                    <div className='flex flex-col gap-1'>
-                        <h3 className='text-center'>HTML5</h3>
-                        <Collapsible>
-                            <></>
-                            <List className='list-disc'>
-                                <ListItem className={listStyle}>
-                                    Began self-taught web development journey learning vanilla HTML/CSS/JS to establish basic understanding.
-                                </ListItem>
-                                <ListItem className={listStyle}>Progressed to learning web template frameworks such as Jinja2.</ListItem>
-                                <ListItem className={listStyle}>
-                                    After establishing basic understanding, developed personal projects using web development frameworks such as
-                                    React.
-                                </ListItem>
-                            </List>
-                        </Collapsible>
-                    </div>
-                </Card>
-                <Card className={cardStyle} image={Logos.CSS} imageSize={{ x: "64px", y: "64px" }} imageClassName='dark:bg-white rounded p-1'>
-                    <div className='flex flex-col gap-1'>
-                        <h3 className='text-center'>CSS3</h3>
-                        <Collapsible>
-                            <></>
-                            <List className='list-disc'>
-                                <ListItem className={listStyle}>
-                                    Developed simple applications with vanilla CSS establishing understanding of basic concepts.
-                                </ListItem>
-                                <ListItem className={listStyle}>Re-factored to use CSS frameworks like Styled Components.</ListItem>
                             </List>
                         </Collapsible>
                     </div>
@@ -242,48 +184,6 @@ function Technologies() {
                 <Card className={cardStyle} image={Logos.debian} imageSize={{ x: "64px", y: "64px" }}>
                     <div className='flex flex-col gap-1'>
                         <h3 className='text-center'>Debian</h3>
-                    </div>
-                </Card>
-                <Card className={cardStyle} image={Logos.powerBI} imageSize={{ x: "64px", y: "64px" }}>
-                    <div className='flex flex-col gap-1'>
-                        <h3 className='text-center'>Power BI</h3>
-                        <Collapsible>
-                            <></>
-                            <List className='list-disc'>
-                                <ListItem className={listStyle}>
-                                    Compiled data for Travel Time routes across the GTA to show in a user-friendly interface.
-                                </ListItem>
-                                <ListItem className={listStyle}>
-                                    Implemented interactive elements to filter various Travel Time routes and respective ITS sensors.
-                                </ListItem>
-                            </List>
-                        </Collapsible>
-                    </div>
-                </Card>
-                <Card className={cardStyle} image={Logos.selenium} imageSize={{ x: "64px", y: "64px" }}>
-                    <div className='flex flex-col gap-1'>
-                        <h3 className='text-center'>Selenium</h3>
-                        <Collapsible>
-                            <></>
-                            <List className='list-disc'>
-                                <ListItem className={listStyle}>
-                                    Evaluated accuracy of automated Intelligent Transportation Systems (ITS) using Selenium.
-                                </ListItem>
-                                <ListItem className={listStyle}>Automated process to compile Travel Times from ON511.</ListItem>
-                            </List>
-                        </Collapsible>
-                    </div>
-                </Card>
-                <Card className={cardStyle} image={Logos.postman} imageSize={{ x: "64px", y: "64px" }}>
-                    <div className='flex flex-col gap-1'>
-                        <h3 className='text-center'>Postman</h3>
-                        <Collapsible>
-                            <></>
-                            <List className='list-disc'>
-                                <ListItem className={listStyle}>Used test-driven development approach to design RestAPI endpoints.</ListItem>
-                                <ListItem className={listStyle}>Tested CRUD operations to cover any potential edge cases.</ListItem>
-                            </List>
-                        </Collapsible>
                     </div>
                 </Card>
             </Grid>
