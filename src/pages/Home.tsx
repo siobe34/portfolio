964a6c6c1cc1b8cb4a01965889c71592a1b23f0b
siobe34 <siobe14@hotmail.com>
2023-02-22 10:09:18 -0500

    re-wrote home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ import { Grid } from "../components/Grid";
 import { Card } from "../components/Card";
 import { Button } from "../components/Button";
 import { FontIcon } from "../components/FontIcon";
-import { LinkCustom } from "../components/LinkCustom";
 
 import imgAbout from "../assets/aboutme.svg";
 import imgProjects from "../assets/projects.svg";
@@ -16,36 +15,28 @@ function Home() {
         <>
             <div className='flex flex-col justify-center items-center mt-4'>
                 <h2>Ibad Rashid</h2>
-                <h4 className='font-medium'>Ryerson Univerity</h4>
+                <h4 className='font-medium'>Ryerson University</h4>
                 <h6 className='font-semibold'>BEng</h6>
             </div>
             <Grid className='gap-8 w-3/5 my-4'>
                 <Card className='flex-col gap-8 p-8' image={imgProjects} imageSize={{ x: "180px", y: "180px" }}>
-                    <Button className='text-xl'>
-                        <LinkCustom to='/projects' className='gap-3 hover:no-underline'>
-                            Check out the projects I've worked on <FontIcon icon={faLongArrowRight} />
-                        </LinkCustom>
+                    <Button className='text-xl gap-3' href='/projects'>
+                        Projects I've worked on <FontIcon icon={faLongArrowRight} />
                     </Button>
                 </Card>
                 <Card className='flex-col gap-8 p-8' image={imgAbout} imageSize={{ x: "180px", y: "180px" }}>
-                    <Button className='text-xl'>
-                        <LinkCustom to='/about-me' className='gap-3 hover:no-underline'>
-                            Read about me <FontIcon icon={faLongArrowRight} />
-                        </LinkCustom>
+                    <Button className='text-xl gap-3' href='/about-me'>
+                        Read about me <FontIcon icon={faLongArrowRight} />
                     </Button>
                 </Card>
                 <Card className='flex-col gap-8 p-8' image={imgTech} imageSize={{ x: "180px", y: "180px" }}>
-                    <Button className='text-xl'>
-                        <LinkCustom to='/technical-skills' className='gap-3 hover:no-underline'>
-                            View the tech stacks I'm familiar with <FontIcon icon={faLongArrowRight} />
-                        </LinkCustom>
+                    <Button className='text-xl gap-3' href='/technical-skills'>
+                        Tech stacks I'm familiar with <FontIcon icon={faLongArrowRight} />
                     </Button>
                 </Card>
                 <Card className='flex-col gap-8 p-8' image={imgResume} imageSize={{ x: "180px", y: "180px" }}>
-                    <Button className='text-xl'>
-                        <LinkCustom to='/resume' className='gap-3 hover:no-underline'>
-                            See a traditional Resume <FontIcon icon={faLongArrowRight} />
-                        </LinkCustom>
+                    <Button className='text-xl gap-3' href='/resume'>
+                        See a traditional Resume <FontIcon icon={faLongArrowRight} />
                     </Button>
                 </Card>
             </Grid>
